dee0ee2ddb7197c740d819b30e58c7f2956489ac
Neha Dixit <ndbo2024@gmail.com>
2026-04-29 14:32:52 -0700

    neha commit - more better wrapped stuff

--- BetterWrapped2.java
+++ BetterWrapped2.java
@@ -12,6 +12,39 @@ public class BetterWrapped2 {
         this.allHistory = MusicDataLoader.CSVAnalysis(fileName);
     }
 
+    //this will be the most important method for feature 1
+    public void analyze(String comparisonType, List<String> midtermDates, List<String> breakDates, List<String> springDates, List<String> summerDates, List<String> fallDates) {
+
+        if (comparisonType.equals("WEEKDAY_VS_WEEKEND")){
+            analyzeWeekdayVsWeekend();
+        }
+        else if (comparisonType.equals("ONE_SEMESTER")){
+            analyzeSemester(midtermDates, breakDates);
+        }
+        else if (comparisonType.equals("FULL_YEAR")){
+            analyzeYear(springDates, summerDates, fallDates);
+        }
+        else{
+            System.out.println("Not a valid time window.");
+        }
+    }
+
+    private void analyzeWeekdayVsWeekend(){
+
+    }
+
+
+    //might do what i did with scrobbling and make a new class that reads a text file of a bunch of dates and simplifies that into a list
+    //also not sure on whether the list should be filled with longs or strings? and with the timestamp stuff too should that be longs or strings?
+    private void analyzeSemester(List<String> midtermDates, List<String> breakDates){
+
+    }
+
+
+    private void analyzeYear(List<String> springDates, List<String> summerDates, List<String> fallDates){
+        
+    }
+
     
 
 
