3c36ebe51dacdc9431df22f3a1b14234c49b1e0a
Neha Dixit <ndbo2024@gmail.com>
2026-04-29 13:53:29 -0700

    nehacommit - added the full csv and a test csv for when we want to test our code, trying to figure out better wrapped

--- BetterWrapped2.java
+++ BetterWrapped2.java
@@ -2,13 +2,27 @@ import java.util.*;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
 
+
+//this will be for all the features?
 public class BetterWrapped2 {
-    private List<MusicEntry> allHistory;
+    //list of all of the listening history: not sure how to upload the data set and squeeze it into this list
+    private List<KeyValuePair> allHistory;
 
     public BetterWrapped2(){
         this.allHistory = new ArrayList<>();
-        
     }
+
+    //create variable that represents the length of the data set?
+    int datasetLength = 0; //not sure how to do this at all. i'm just generally not sure how we'd even be able to read the data set and put in all of the timestamps
+
+    //method for adding the songs one by one into the allHistory list
+    public void addEntry(){ //this might need to be in a different class overall
+
+    } 
+
+
+
+
 
 
 
