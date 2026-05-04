c18f05683b98f033674fa2e6e2ef6df04f3653ef
stephanienguyn <haphuong132006@gmail.com>
2026-05-03 18:10:41 -0700

    kinda finished feature 1

--- BetterWrapped2.java
+++ BetterWrapped2.java
@@ -1,7 +1,6 @@
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.*;
-import java.time.*;
-import java.time.temporal.ChronoUnit;
-
 
 //this will be for all the features?
 public class BetterWrapped2 {
@@ -13,7 +12,7 @@ public class BetterWrapped2 {
     }
 
     //this will be the most important method for feature 1
-    public void analyze(String comparisonType, List<String> midtermDates, List<String> breakDates, List<String> springDates, List<String> summerDates, List<String> fallDates) {
+    public void analyze(String comparisonType, List<Timestamp> midtermDates, List<Timestamp> breakDates, List<Timestamp> springDates, List<Timestamp> summerDates, List<Timestamp> fallDates) {
 
         if (comparisonType.equals("WEEKDAY_VS_WEEKEND")){
             analyzeWeekdayVsWeekend();
@@ -33,35 +32,60 @@ public class BetterWrapped2 {
 
     }
 
-
-    //might do what i did with scrobbling and make a new class that reads a text file of a bunch of dates and simplifies that into a list
-    //also not sure on whether the list should be filled with longs or strings? and with the timestamp stuff too should that be longs or strings?
-    private void analyzeSemester(List<String> midtermDates, List<String> breakDates){
-        List<MusicEntry> midtermList = new ArrayList<>();
-        List<MusicEntry> breakList = new ArrayList<>();
-        List<MusicEntry> normalList = new ArrayList<>();
+    private void analyzeSemester(Timestamp midtermDates, Timestamp breakDates){
+        List<KeyValuePair> midtermList = new ArrayList<>();
+        List<KeyValuePair> breakList = new ArrayList<>();
+        List<KeyValuePair> normalList = new ArrayList<>();
 
         //they input the date into the console, and then we put that into a list of our own to classify it as a midterm date
 
+        for (KeyValuePair entry : allHistory) {
+            Timestamp songTime = entry.getTimeStamp();
 
+            if (isWithinWindow(songTime, midtermDates, 5)) {
+                midtermList.add(entry);
+            } else if (isWithinWindow(songTime, breakDates, 5)) {
+                breakList.add(entry);
+            } else {
+                normalList.add(entry);
+            }
+            }
 
-
-
+        System.out.println("======= MIDTERM PERIOD STATS =======");
+        SongStatistics midtermStats = new SongStatistics(midtermList);
+        System.out.println(midtermStats);
 
     }
 
+    /**
+     * Checks if a song was played within a specific number of days BEFORE a target date.
+    */
 
-    private void analyzeYear(List<String> springDates, List<String> summerDates, List<String> fallDates){
+    private boolean isWithinWindow(Timestamp songTime, Timestamp targetDate, int daysBefore) {
+        LocalDateTime ldt = targetDate.toLocalDateTime();
+        LocalDateTime startDateLDT = ldt.minusDays(daysBefore);
+        Timestamp startDate = Timestamp.valueOf(startDateLDT);
+
+        return songTime.after(startDate) && songTime.before(targetDate);
+
+    }
+
+    // private void analyzeYear(List<String> springDates, List<String> summerDates, List<String> fallDates){
+    //     pass;
+    // }
+
+    public static void main(String[] args) {
+        // 1. Create the instance
+        BetterWrapped2 myWrapped = new BetterWrapped2("testScrobbles.csv");
+
+        // 2. Create sample test dates (Midterms on 9 29, Break on Nov 24)
+        Timestamp testMidterm = Timestamp.valueOf(LocalDateTime.of(2023, 9, 29, 23, 59));
+        Timestamp testBreak = Timestamp.valueOf(LocalDateTime.of(2023, 11, 24, 23, 59));
+
+        // 3. Run the test
+        System.out.println("Starting Analysis...\n");
+        myWrapped.analyzeSemester(testMidterm, testBreak);
+    }   
         
-    }
+    } 
 
-    
-
-
-
-
-
-
-
-
-}
--- SongStatistics.java
+++ SongStatistics.java
@@ -1,44 +1,98 @@
 
 
 //This will be part of feature 1. It takes in a data set and outputs summary statistics
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.sql.Timestamp;
+
 public class SongStatistics {
     private String topArtist;
     private String topSong;
     private String topGenre;
     private double totalMinutes; //should we use this?
 
-    public SongStatistics (String topArtist, String topSong, String topGenre, double totalMinutes){
-        this.topArtist = topArtist;
-        this.topSong = topSong;
-        this.topGenre = topGenre;
-        this.totalMinutes = totalMinutes;
+    public SongStatistics (List<KeyValuePair> list){
+        if (list == null || list.isEmpty()) {
+            this.topArtist = "None";
+            this.topSong = "None";
+            this.topGenre = "None";
+            return;
+        }
+
+        Map<String, Integer> artistCounts = new HashMap<>();
+        Map<String, Integer> songCounts = new HashMap<>();
+        Map<String, Integer> genreCounts = new HashMap<>();
+
+        //Fill in the value in hashMaps
+        for (KeyValuePair entry : list) {
+            String artist = entry.getSongObject().getArtist();
+            String songName = entry.getSongObject().getName();
+            String genre = entry.getSongObject().getGenre();
+
+            //Update frequency counts
+            artistCounts.put(artist, artistCounts.getOrDefault(artist, 0) + 1);
+            songCounts.put(songName, songCounts.getOrDefault(songName, 0) + 1);
+            genreCounts.put(genre, genreCounts.getOrDefault(genre, 0) + 1);
+        }
+
+        this.topArtist = findMax(artistCounts);
+        this.topSong = findMax(songCounts);
+        this.topGenre = findMax(genreCounts);
+
+
+        }
+
+    private String findMax(Map<String, Integer> counts) {
+        if (counts.isEmpty()) {
+            return "None";
+        }
+
+        Map.Entry<String, Integer> winner = Collections.max(counts.entrySet(), Map.Entry.comparingByValue());
+        return winner.getKey();
     }
 
 
     public String toString(){
-        return "Top artist: " + topArtist + "\nTop song: " + topSong +"\nTop genre: " + topGenre + "\nMinutes listened: " + totalMinutes;
+        // To be edited
+        return String.format("Top Artist: %s\nTop Song: %s\nTop Genre: %s\n", 
+                             topArtist, topSong, topGenre);
     }
 
+    public static void main(String[] args) {
+        System.out.println("--- Testing SongStatistics Frequency Analysis ---");
 
+        // 1. Create dummy SongInfo objects
+        SongInfo dua1 = new SongInfo("Dua Lipa", "Levitating", "Pop");
+        SongInfo dua2 = new SongInfo("Dua Lipa", "Physical", "Pop");
+        SongInfo weeknd = new SongInfo("The Weeknd", "Blinding Lights", "R&B");
 
+        // 2. Create a dummy list of KeyValuePairs
+        List<KeyValuePair> testList = new ArrayList<>();
+    
+        // We'll add Dua Lipa 3 times, The Weeknd 1 time
+        // We'll add "Levitating" 2 times, "Physical" 1 time
+        // Pop will have 3 counts, R&B will have 1
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+    
+        testList.add(new KeyValuePair(now, dua1));
+        testList.add(new KeyValuePair(now, dua1)); // Repeat song
+        testList.add(new KeyValuePair(now, dua2)); // Same artist, different song
+        testList.add(new KeyValuePair(now, weeknd));
 
+        // 3. Initialize SongStatistics
+        // This will trigger your HashMap logic
+        SongStatistics stats = new SongStatistics(testList);
 
+        // 4. Verify Results
+        System.out.println(stats.toString());
 
+        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 
